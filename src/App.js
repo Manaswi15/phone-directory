51ad207c2c255259c9e53eb3c64c86d6f05516fa
unknown <sanama87@gmail.com>
2019-05-01 13:51:45 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        Phone directory
+        <Header></Header>
+        <button>Add</button><br/>
+        <label htmlFor="name">Name:</label>
+        <input type="text" placeholder="Type here"></input><br/>
+        <label>Phone:</label>
+      <input type="text"></input>
       </div>
     );
   }
