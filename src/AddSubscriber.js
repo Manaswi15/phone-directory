660260f020bce4dd42a47f1f4336fbfb20d0271f
unknown <sanama87@gmail.com>
2019-05-04 19:58:28 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 //import './AddSubscriber.css';
+import {Link} from 'react-router-dom';
 
 class AddSubscriber extends Component {
     constructor(){
@@ -21,6 +22,14 @@ class AddSubscriber extends Component {
         console.log(this.state);
 
     }
+
+    onFormSubmitted = (e) => {
+        e.preventDefault();
+        this.props.addSubscriberHandler(this.state);
+        this.setState({ id: 0, name: '', phone: ' ' });
+        this.props.history.push("/");
+    }
+
     render() {
         const {name,phone} = this.state; 
 
@@ -29,8 +38,10 @@ class AddSubscriber extends Component {
             <div>
                 <Header heading="Add Subscriber" />
                 <div className="component-body-container">
-                <button className="custom-btn">BACK</button>
-                <form className="subscriber-form">
+                <Link to="/">
+                        <button className="custom-btn">Back</button>
+                </Link>
+                <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
                         <label htmlFor="name" className="label-control">Name: </label><br/>
                         <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangeHandler} /><br/><br/>
                         <label htmlFor="phone" className="label-control">Phone: </label><br/>
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,55 +1,32 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import './App.css';
+import Header from './Header.js';
+import './ShowSubscribers.css';
+import { Link } from 'react-router-dom';
 
-
-class App extends Component {
-
-  /*let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];*/
-
-    construtcor() {
-
-      super();
-      this.state = {
-        subscribersListToShow:[]
-      }
-
-    }
-
-  clickHandler(message)
-    {
-      alert(message);
-    }
+class ShowSubscribers extends Component {
 
   render() {
-   
+
     return (
       <div>
         <Header heading="Phone Directory" />
         <div className="component-body-container">
-          <button className="custom-btn add-btn">Add</button>
+          <Link to="/add">
+            <button className="custom-btn add-btn">Add</button>
+          </Link>
+
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+
           {
-            this.subscribersListToShow.map(sub => {
+            this.props.subscribersList.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
                 <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn" onClick={this.clickHandler.bind(this,"Delete button clicked")}>Delete</button>
+                  <button className="custom-btn delete-btn">Delete</button>
                 </span>
               </div>
             })
@@ -60,4 +37,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default ShowSubscribers;
